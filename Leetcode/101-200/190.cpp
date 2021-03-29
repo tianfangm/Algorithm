60b621705555cc643a56585ac9956c09390bca63
tianfangm <81548245+tianfangm@users.noreply.github.com>
2021-03-29 15:37:24 +0800

    Create 190.cpp

---
+++ Leetcode/101-200/190.cpp
@@ -0,0 +1,13 @@
+// 位运算
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ret = 0;
+        for(int i =0;i<32&&n>0;++i)
+        {
+            ret |= (n&1)<<(31-i);
+            n>>=1;
+        }
+        return ret;
+    }
+};
