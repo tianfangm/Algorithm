6cfe998960ef0cc4d5e0ac03b2d5a003c5dd92fa
tianfangm <81548245+tianfangm@users.noreply.github.com>
2021-04-04 14:56:34 +0800

    Create 144.cpp

---
+++ Leetcode/101-200/144.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*>stk;
+        vector<int>result;
+        while(root||!stk.empty())
+        {
+            while(root)
+            {
+                stk.push(root);
+                result.push_back(root->val);
+                root=root->left;
+            }
+            root = stk.top();
+            stk.pop();
+            root = root -> right;
+        }
+        return result;
+    }
+};
