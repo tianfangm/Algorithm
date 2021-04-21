fa5961dffea4c85a5ee51b89ed4a586a9e069aca
tianfangm <81548245+tianfangm@users.noreply.github.com>
2021-04-21 13:17:47 +0800

    Create 91.cpp

---
+++ Leetcode/1-100/91.cpp
@@ -0,0 +1,23 @@
+// 动态规划
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        // a = f[i-2],b = f[i-1],c
+        int a = 0,b = 1,c;
+        for(int i = 1;i<=n;++i)
+        {
+            c =  0;
+            if(s[i-1]!='0')
+            {
+                c+=b;
+            }
+            if(i>1&&s[i-2]!='0'&&((s[i-2]-'0')*10+(s[i-1]-'0')<=26))
+            {
+                c+=a;
+            }
+            tie(a,b)={b,c};
+        }
+        return c;
+    }
+};
